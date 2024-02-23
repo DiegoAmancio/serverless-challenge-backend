bf0cb07050e36860664650fb141923f4eb31ea4f
Diego Amancio <diego.pereira@ccc.ufcg.edu.br>
2024-02-23 14:55:19 -0300

    feat: validation pipe
    add validation pipe globally

--- src/lambda.ts
+++ src/lambda.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { configure as serverlessExpress } from '@vendia/serverless-express';
+import { ValidationPipe } from '@nestjs/common';
 
 let cachedServer: any;
 export const handler = async (event: any, context: any) => {
   if (!cachedServer) {
     const nestApp = await NestFactory.create(AppModule);
+    nestApp.useGlobalPipes(new ValidationPipe({ whitelist: true }));
     await nestApp.init();
     cachedServer = serverlessExpress({
       app: nestApp.getHttpAdapter().getInstance(),
--- test/employee.test.ts
+++ test/employee.test.ts
@@ -2,40 +2,52 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { EMPLOYEE_REPOSITORY, EMPLOYEE_SERVICE } from '@shared/injects';
 import { EmployeeRepositoryImpl } from '@domain/employee';
 import { EmployeeService } from '@service/index';
+import { EmployeeController } from '@controller/employee';
+import { EmployeeEntity } from '@repository/employee/entity';
 
-describe('EmployeeService', () => {
-  let service: EmployeeService;
-
+describe('Employee', () => {
+  let controller: EmployeeController;
+  const defaultEmployee = {
+    Idade: "25",
+    Nome: "Top",
+    Cargo: "DEVELOP",
+    id: EmployeeEntity.getPK('123')
+  }
   const mockRepository: EmployeeRepositoryImpl = {
-    create: jest.fn().mockReturnValue(null),
+    create: jest.fn().mockReturnValue(defaultEmployee),
   };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      controllers: [EmployeeController],
       providers: [
-
         {
           provide: EMPLOYEE_REPOSITORY,
           useValue: mockRepository,
         },
-        EmployeeService
+        {
+          provide: EMPLOYEE_SERVICE,
+          useClass: EmployeeService,
+        },
       ],
     }).compile();
-    service = module.get<EmployeeService>(EmployeeService);
+
+    controller = module.get<EmployeeController>(EmployeeController);
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(controller).toBeDefined();
   });
 
   describe('When create', () => {
-    it('should be login in system (is registered)', async () => {
+    it('should be can create employee', async () => {
+
       const body = {
-        Idade: "21",
-        Nome: "Pops5",
-        Cargo: "DEVELOP"
+        Cargo: defaultEmployee.Cargo,
+        Idade: defaultEmployee.Idade,
+        Nome: defaultEmployee.Nome
       }
-      const employee = await service.create(body);
+      const employee = await controller.create(body);
 
       expect(mockRepository.create).toHaveBeenCalledWith({
         Idade: body.Idade,
@@ -43,6 +55,10 @@ describe('EmployeeService', () => {
         Cargo: body.Cargo,
       });
       expect(mockRepository.create).toHaveBeenCalledTimes(1);
+      expect(employee).toStrictEqual({
+        statusCode: 201,
+        body: defaultEmployee
+      })
     });
   });
 });
